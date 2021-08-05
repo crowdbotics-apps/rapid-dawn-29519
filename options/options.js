27c38cbb010f1435ba29bc2c74bf5940ab2945e2
Crowdbotics <team@crowdbotics.com>
2021-08-05 16:46:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_dawn_29519Identifier",
+  url: "https://rapid_dawn_29519Identifier.botics.co",
+  api: "https://rapid_dawn_29519Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
